aa58b46ae0b19d2cc2ba9d3f322eeb9acb011f03
Jonathan <unkownvazguz@gmail.com>
2024-04-24 21:38:54 -0600

    Fix: Variable cannot be imported to another file, working on a config init function or instead finda way to pass arguments on CLI target exec

--- src/http/connect.zig
+++ src/http/connect.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
+const config = @import("../main.zig");
+
 const print = std.debug.print;
 const http = std.http;
-const config = @import("../main.zig").config;
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
@@ -10,7 +11,8 @@ pub fn connnectByGet() !void {
     var client = http.Client{ .allocator = allocator };
     defer client.deinit();
 
-    const uri = try std.Uri.parse("http://httpbin.org/headers");
+    const host = try config.initConfig();
+    const uri = try std.Uri.parse(host);
     const buf = try allocator.alloc(u8, 1024 * 1024 * 4);
     defer allocator.free(buf);
     var req = try client.open(.GET, uri, .{
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const cli = @import("./modules/cli/main.zig");
-const get = @import("./http/connect.zig");
+
+const print = std.debug.print;
+const http = std.http;
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
@@ -23,10 +25,40 @@ var app = &cli.App{
         .description = cli.Description{
             .one_line = "Download a file from the web",
         },
-        .target = cli.CommandTarget{ .action = cli.CommandAction{ .exec = get.connnectByGet } },
+        .target = cli.CommandTarget{ .action = cli.CommandAction{ .exec = connnectByGet } },
     },
 };
 
 pub fn main() !void {
     return cli.run(app, allocator);
+}
+
+pub fn connnectByGet() !void {
+    var client = http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    const uri = try std.Uri.parse(config.host);
+    const buf = try allocator.alloc(u8, 1024 * 1024 * 4);
+    defer allocator.free(buf);
+    var req = try client.open(.GET, uri, .{
+        .server_header_buffer = buf,
+    });
+    defer req.deinit();
+
+    try req.send();
+    try req.finish();
+    try req.wait();
+
+    var iter = req.response.iterateHeaders();
+    while (iter.next()) |header| {
+        std.debug.print("Name:{s}, Value:{s}\n", .{ header.name, header.value });
+    }
+
+    try std.testing.expectEqual(req.response.status, .ok);
+
+    var rdr = req.reader();
+    const body = try rdr.readAllAlloc(allocator, 1024 * 1024 * 4);
+    defer allocator.free(body);
+
+    print("Body:\n{s}\n", .{body});
 }
